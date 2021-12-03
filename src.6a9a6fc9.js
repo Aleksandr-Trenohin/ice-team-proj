c52a07b075ed1ad191cec3cc40efc0975a68952c
Aleksandr-Trenohin <90101201+Aleksandr-Trenohin@users.noreply.github.com>
2021-12-03 17:51:31 +0000

    Deploying to gh-pages from @ Aleksandr-Trenohin/ice-team-proj@7ec8240e8f7b89efb7c6af5bf4605d9505a0f0db 🚀

--- src.6a9a6fc9.js
+++ src.6a9a6fc9.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/ice-team-proj/src.e634de22.js.map
+//# sourceMappingURL=/ice-team-proj/src.6a9a6fc9.js.map
